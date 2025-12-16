42c2429d64d36fca21eb432ffbaa4c098ebe332c
fire8327 <fire83274@gmail.com>
2025-12-17 01:13:47 +0300

    add tailwind

---
+++ app/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <!-- <Header></Header> -->
+    <main class="grid-container grow my-16 gap-y-16">
+        <slot/>
+    </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/components/**/*.{js,vue,ts}",
+      "./app/layouts/**/*.vue",
+      "./app/pages/**/*.vue",
+      "./app/plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+      "./app/app.vue",
+    ],
+    theme: {
+      extend: {
+        /* fontFamily: {
+          'montserrat': ['Montserrat', 'sans-serif'],
+        }, */
+      },
+    },
+    plugins: [],
+  }
