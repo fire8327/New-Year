327cfcd2d2863538a6acdb487a951c2720bf97c5
fire8327 <fire83274@gmail.com>
2025-12-27 00:00:11 +0300

    upd

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!passwordStore.password || (passwordStore.password != 1 && passwordStore.password != 2 && passwordStore.password != 3)" class="grow flex items-center justify-center">
+    <div v-if="!passwordStore.password || (passwordStore.password != '7563' && passwordStore.password != '4547' && passwordStore.password != '0499')" class="grow flex items-center justify-center">
         <div class="w-[calc(100%-16px)] md:w-1/2 lg:w-1/3 p-4 border border-white/10 rounded-xl flex flex-col gap-6">
             <p class="text-xl font-mono tracking-wide font-medium text-center">Введите пароль</p>
             <FormKit v-model="passwordStore.password" type="password" outer-class="w-full" input-class="w-full px-4 py-1.5 rounded-xl text-sm font-medium bg-transparent border border-white/15 focus:border-white/50 focus:outline-none" placeholder="Пароль"/>
